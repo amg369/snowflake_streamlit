7d7578d31616184dc59cff5c91f1d1048b804901
Aliyah Geer <43519256+amg369@users.noreply.github.com>
2023-07-11 15:41:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('Pippy is a chunky monkey')
 
-streamlit.header('Pippy's Breakfast Menu')
+streamlit.header("Pippy's Breakfast Menu")
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
