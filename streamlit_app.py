de08eb452d849299663b225df9bc916a908cdcd9
Aliyah Geer <43519256+amg369@users.noreply.github.com>
2023-07-11 15:53:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
